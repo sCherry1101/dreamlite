df304def58d36c2a6f2bcf493a1ba51a95663fd0
sCherry1101 <cherry.1101.8113@gmail.com>
2026-05-17 17:06:30 +0530

    build: file rename , css , working demo for 3d in div

--- src/cellorganelles.js
+++ src/cellorganelles.js
@@ -1,18 +1,60 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-//cell
-const container = document.getElementById('canva_cell');
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 
-renderer.setSize(container.clientWidth, container.clientHeight);
-container.appendChild(renderer.domElement);
+// the DIV from the page
 
-const light = new THREE.AmbientLight(0xffffff, 1);
-scene.add(light);
+var container = document.getElementById( 'nucleus_dia' );
 
-const geometry = new THREE.ShepreGeometry(1, 32,32);
-const material = new THREE.MeshStandardMaterial({ color: 0x00ff00, wireframe: true });
-const cell = new THREE.Mesh(geometry, material);
-scene.add(cell);
+
+// a scene
+
+var scene = new THREE.Scene();
+    scene.background = new THREE.Color( 'gainsboro' );
+
+
+// camera that uses the container's size
+
+var camera = new THREE.PerspectiveCamera( 30, container.clientWidth/ container.clientHeight );
+    camera.position.set( 2, 5, 10 );
+    camera.lookAt( scene.position );
+
+
+// renderer that uses the container's size and is inserted in it
+
+var renderer = new THREE.WebGLRenderer( {antialias: true} );
+    renderer.setSize( container.clientWidth-16, container.clientHeight-16 );
+    renderer.setAnimationLoop( animationLoop );
+    container.appendChild( renderer.domElement );
+
+
+// various stuff, not interesting
+
+var controls = new OrbitControls( camera, renderer.domElement );
+    controls.enableDamping = true;
+
+var ambientLight = new THREE.AmbientLight( 'white', 0.5 );
+    scene.add( ambientLight );
+
+var light = new THREE.DirectionalLight( 'white', 0.5 );
+    light.position.set( 1, 1, 1 );
+    scene.add( light );
+
+var cube = new THREE.Mesh(
+			new THREE.BoxGeometry( 1, 1, 1 ),
+    	new THREE.MeshPhongMaterial( {color:'tan', shininess: 10} )
+    );	
+		scene.add( cube );
+
+scene.add( new THREE.AxesHelper(2) );
+
+
+function animationLoop( t )
+{
+    cube.rotation.x = Math.sin( t/700 );
+    cube.rotation.y = Math.sin( t/900 );
+
+    controls.update( );
+		light.position.copy( camera.position );
+    renderer.render( scene, camera );
+}
