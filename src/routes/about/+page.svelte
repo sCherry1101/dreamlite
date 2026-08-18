fd6b8cb482d9ffbe01e07162806860ae9c6f8d61
sCherry1101 <cherry.1101.8113@gmail.com>
2026-08-18 18:11:00 +0530

    build:basic about page and fixing whitespaces in css

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,16 @@
-<div id="error_div">
+<div id="about_div">
   
-  <h2>This module is in progress and it will be coming soon!</h2>
-  <a href="https://github.com/sCherry1101/dreamlite/">Click to check the repository instead for updates!</a>
+  <h1>About Dreamlite</h1>
+  <p>Dreamlite is a educational platform for learning various STEM based diagrams visualised in 3D and interactive diagrams to learn better. </p>
+  
+  <h1>Contributions</h1>
+    <p>Dreamlite is an open source project and contributions are welcome. You can contribute </p>
+
+  <h1>Contributors</h1>>
+
+  <ul>
+    <li><
+        <a href="https://github.com/sCherry1101">@sCherry1101</a>
+        <a href="https://github.com/chwrryroll">@chwrryroll</a>
+        <a href="https://github.com/jagath-sajjan">@jagath-sajjan</a>
 </div>
